b0e6a0f1548be7bda4aa8babdd7c21e79a4688e9
Brian Whippo <brian.whippo@algorand.foundation>
2024-12-05 17:03:04 -0500

    Min repro of simulate failure on rekeyed account

--- projects/rekeyed/__test__/HelloWorld.test.ts
+++ projects/rekeyed/__test__/HelloWorld.test.ts
@@ -13,31 +13,46 @@ describe('HelloWorld', () => {
 
   beforeAll(async () => {
     await fixture.beforeEach();
-    const { testAccount } = fixture.context;
+    const { testAccount: firstAccount, generateAccount } = fixture.context;
     const { algorand } = fixture;
+
+    const secondAccount = await generateAccount({ initialFunds: (100).algos() });
+
+    console.debug('First account: ', firstAccount.addr.toString());
+    console.debug('Second account: ', secondAccount.addr.toString());
+
+    const rekeyResult = await algorand.send.payment({
+      amount: (0).algos(),
+      sender: firstAccount.addr,
+      receiver: firstAccount.addr,
+      rekeyTo: secondAccount.addr,
+      note: 'Rekeying test account to second account',
+    });
+    // console.debug('Rekey result:', rekeyResult);
 
     const factory = new HelloWorldFactory({
       algorand,
-      defaultSender: testAccount.addr,
+      defaultSender: firstAccount.addr,
+      defaultSigner: secondAccount.signer,
     });
 
     const createResult = await factory.send.create.createApplication();
     appClient = createResult.appClient;
   });
 
-  test('sum', async () => {
-    const a = 13;
-    const b = 37;
-    const sum = await appClient.send.doMath({ args: { a, b, operation: 'sum' } });
-    expect(sum.return).toBe(BigInt(a + b));
-  });
+  // test('sum', async () => {
+  //   const a = 13;
+  //   const b = 37;
+  //   const sum = await appClient.send.doMath({ args: { a, b, operation: 'sum' } });
+  //   expect(sum.return).toBe(BigInt(a + b));
+  // });
 
-  test('difference', async () => {
-    const a = 13;
-    const b = 37;
-    const diff = await appClient.send.doMath({ args: { a, b, operation: 'difference' } });
-    expect(diff.return).toBe(BigInt(a >= b ? a - b : b - a));
-  });
+  // test('difference', async () => {
+  //   const a = 13;
+  //   const b = 37;
+  //   const diff = await appClient.send.doMath({ args: { a, b, operation: 'difference' } });
+  //   expect(diff.return).toBe(BigInt(a >= b ? a - b : b - a));
+  // });
 
   test('hello', async () => {
     const hello = await appClient.send.hello({ args: { name: 'world!' } });
